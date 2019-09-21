09d0cdf8c67d59c367023e27cccd4ca66182fa5a
Alex Francev <18609701+avfrancev@users.noreply.github.com>
2019-09-21 14:35:53 +0300

    Main styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-responsive-type': {},
+    // 'postcss-vertical-rhythm': {},
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,22 @@
 </template>
 
 <style lang="stylus">
-  // @import 'https://meyerweb.com/eric/tools/css/reset/reset.css';
+  @import 'https://meyerweb.com/eric/tools/css/reset/reset.css';
+  @import '~@/styles/main.styl'
+  // html, body
+  //   overflow-x: hidden
   body
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing grayscale
-    text-align center
-    color c1
+    // text-align center
+    color c2
     margin: 0
+    // font: 16px/2 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
+    // line-height: responsive 1.6rem 1.8rem
+    // line-height: responsive
+    // font-size: responsive 1.2rem 2.3rem
+    // font-size: 32px/2
+
+
 </style>
